6de3d28c1cfbd6f517d693b5245644097d154d94
Peter <peter.gschirr@gmail.com>
2017-05-09 11:01:00 +0200

    Smoothing

--- transform_data.R
+++ transform_data.R
@@ -1,7 +1,7 @@
 require(anytime)
 require(dplyr)
 
-working_dir <- "C:/Users/DE-98128/Privat/tradedata-master/"
+working_dir <- "/home/peter/PycharmProjects/mefirstproject/"
 
 trade_data <- read.csv(paste0(working_dir,"XETHZEUR.csv"),header = FALSE)
 names(trade_data) <- c("Price","Volume","Timestamp","Buy_Sell","Market_Limit","X")
@@ -16,6 +16,25 @@ trade_data <- trade_data %>% mutate(Acc_Volume=cumsum(Volume)) %>%
   mutate(Acc_Sell_Volume=cumsum(Sell_Volume)) %>%
   mutate(Acc_Buy_Volume=cumsum(Buy_Volume)) %>%
   mutate(Diff_Volume=Acc_Buy_Volume-Acc_Sell_Volume) %>%
-  select(-Sell_Volume,-Buy_Volume)
+  select(-Sell_Volume,-Buy_Volume,-Timestamp)
 
-write.csv(paste0(working_dir,"XETHZEUR_transformed.csv"))
+trade_data$Price_sm <- rollmean(trade_data$Price,k = 200,fill = c(trade_data$Price[1],1,trade_data$Price[length(trade_data$Price)]))
+
+trade_data$Diff_Volume_sm <- rollmean(trade_data$Diff_Volume,k = 200,fill = c(trade_data$Diff_Volume[1],1,trade_data$Diff_Volume[length(trade_data$Diff_Volume)]))
+
+
+write.csv(trade_data,paste0(working_dir,"XETHZEUR_transformed.csv"),row.names = FALSE)
+
+qplot(data=trade_data[300000:340000,],Time,Price,geom="line") + geom_line(aes(Time,Diff_Volume/5e+4+10),col="red")
+
+
+
+smoothed_price <- apply(trade_data,1,function(x){ mn <- mean(trade_data[trade_data$Time<=x[5] & trade_data$Time>=x[5]-300,1]); ifelse(is.na(mn),x[1],mn)})
+
+
+smoothed_price <- apply(trade_data[300000:310000,],1,function(x){ mn <- mean(trade_data[trade_data$Time<=as.POSIXlt(x[5]) & trade_data$Time>=as.POSIXlt(x[5])-3600,1]); ifelse(is.na(mn),x[1],mn)})
+
+deriv_price <- (diff(trade_data[700000:703000,]$Price_sm,lag=1)/trade_data[700000:702999,]$Price_sm)
+deriv_vol <- (diff(trade_data[700000:703000,]$Diff_Volume_sm,lag=1)/trade_data[700000:702999,]$Diff_Volume_sm)
+deriv_price_sm <- rollmean(deriv_price,k = 200,fill = c(deriv_price[1],1,deriv_price[length(deriv_price)]))
+deriv_vol_sm <- rollmean(deriv_vol,k = 200,fill = c(deriv_vol[1],1,deriv_vol[length(deriv_vol)]))
